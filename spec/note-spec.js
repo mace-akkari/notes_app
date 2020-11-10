2cf5ad327d4d3e5978e236462ae3b87dc7e99b5c
Mace <maceplus@gmail.com>
2020-11-10 12:05:58 +0000

    Restructured files.

--- spec/note-spec.js
+++
@@ -1,8 +0,0 @@
-( () => {
-
-  test.that('getNote returns the note text', () => {
-    let note = new Note('I am with Ben!');
-    expect.toEqual(note.getNote, 'I am with Ben!');
-  });
-
-})(this);
