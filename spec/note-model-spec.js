fbb1237aa17c610600f7b05ffab3c85ea380d29e
Mace <maceplus@gmail.com>
2020-11-10 11:57:27 +0000

    Refactored and Re-Structured Asparagus Testing Suite with Ben.

--- spec/note-model-spec.js
+++ spec/note-model-spec.js
@@ -1,36 +1,6 @@
-class Asparagus {
-
-  constructor(argument) {
-    this.argument = argument;
-  }
-
-  toEqual(expected) {
-    return this._equalMatcher(expected);
-  }
-
-  toBeAnInstanceOf(className) {
-    return this._instanceTypeMatcher(className);
-  }
-
-  toBeAn(className) {
-    return this._instanceTypeMatcher(className);
-  }
-
-  toBeA(className) {
-    return this._instanceTypeMatcher(className);
-  }
-
-
-  _instanceTypeMatcher(className) {
-    return this.argument instanceof className;
-
-  }
-
-  _equalMatcher(expected) {
-    return this.argument === expected;
-  }
-}
-
-function expect(argument) {
-  return new Asparagus(argument);
-}
+( () => {
+  test.that('The message is the same', () => {
+    let note = new Note("I am with mace!");
+    expect.toEqual(note.showMessage(), "I am with Mace!");
+  })
+})(this);
