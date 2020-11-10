f9346e6a618471aed71e2d3ab70d03968c8aa413
Mace <maceplus@gmail.com>
2020-11-10 12:17:53 +0000

    feat: Tests instance of class

--- spec/note-model-spec.js
+++ spec/note-model-spec.js
@@ -1,6 +1,11 @@
-( () => {
-  test.that('The message is the same', () => {
-    let note = new Note("I am with mace!");
+(() => {
+  test.that("It is an instance of a class", () => {
+    let note = new Note("testNote");
+    expect.toBeAnInstanceOf(note, Note);
+  });
+
+  test.that("The message is the same", () => {
+    let note = new Note("I am with Mace!");
     expect.toEqual(note.showMessage(), "I am with Mace!");
-  })
+  });
 })(this);
