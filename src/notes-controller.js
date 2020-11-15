46da707a2278d7e7618b9eb8369dcb32c1ee9c62
Mace <maceplus@gmail.com>
2020-11-15 01:01:29 +0000

    renaming of files & basic styling

--- src/notes-controller.js
+++ src/notes-controller.js
@@ -1,5 +1,6 @@
-let originalCreateFormDiv = document.getElementById('createFormDiv').innerHTML;
+'use strict'
 
+let originalCreateFormDiv = document.getElementById("createFormDiv").innerHTML;
 
 let noteListView = new NoteListView();
 let notes = new Notes();
@@ -7,36 +8,46 @@ let notes = new Notes();
 notes.addNote("'Ruby sucks' - Mace");
 notes.addNote("'Ruby is cool' - Ben");
 
-let appDiv = document.getElementById('app');
+let appDiv = document.getElementById("app");
 
 function updateNotesList() {
   appDiv.innerHTML = noteListView.buildHTML(notes);
 };
 
 (function startingWebpage() {
-  updateNotesList()
+  updateNotesList();
 })(this);
 
 function viewNote(noteID) {
-  let noteView = new SingleNoteView(notes.noteList[noteID-1]);
-  let previewDiv = document.getElementById('preview');
+  let noteView = new SingleNoteView(notes.noteList[noteID - 1]);
+  let previewDiv = document.getElementById("preview");
   previewDiv.innerHTML = noteView.buildHTML();
 };
 
-document.getElementById('createForm').addEventListener('submit', function createNoteForm(e) {
-  e.preventDefault();
-  document.getElementById('createFormDiv').innerHTML = "";
-  document.getElementById('preview').innerHTML = "<form id='createNote'><ul><li><textarea id='createNoteText' name='note' placeholder='Enter new note: '></textarea></li><li><input id='createNoteSubmit' type='submit' value='Add Note'></form>";
-  eventSetupToCreateNote()
-});
+function noteForm() {
+  document
+    .getElementById("createForm")
+    .addEventListener("submit", function createNoteForm(e) {
+      e.preventDefault();
+      document.getElementById("createFormDiv").innerHTML = "";
+      document.getElementById("preview").innerHTML =
+        "<form id='createNote'><ul><li><textarea id='createNoteText' name='note' placeholder='New note: '></textarea></li><li><input id='createNoteSubmit' type='submit' value='Add Note'></form>";
+      eventSetupToCreateNote();
+    });
+}
+noteForm();
 
 function eventSetupToCreateNote() {
-  document.getElementById('createNote').addEventListener('submit', function createNote(e) {
-    e.preventDefault();
-    notes.addNote(document.getElementById('createNoteText').value);
-    document.getElementById('createFormDiv').innerHTML = originalCreateFormDiv;
-    document.getElementById('preview').innerHTML = "";
-    updateNotesList()
-  });
-
+  document
+    .getElementById("createNote")
+    .addEventListener("submit", function createNote(e) {
+      e.preventDefault();
+      notes.addNote(document.getElementById("createNoteText").value);
+      document.getElementById(
+        "createFormDiv"
+      ).innerHTML = originalCreateFormDiv;
+      document.getElementById("preview").innerHTML = "";
+      updateNotesList();
+      noteForm();
+    });
 };
--- src/notes-list-view.js
+++ src/notes-list-view.js
@@ -3,12 +3,13 @@
 class NoteListView {
 
   buildHTML(noteList) {
-    let startUl = '<ul>'
-    let endUl = '</ul>'
+    let startUl = '<ul>';
+    let endUl = '</ul>';
+    // [startUl, endUl].forEach((i) => console.log(i)); Line above will cause this to fail because of lack of semicolon
     let noteArr = noteList.getNotes().map((note, i) => {
       return `<li><a class='note${i+1}' onClick='viewNote(${i + 1})' href='#'>${note.showNote().slice(0,20)}</a></li>`
     });
     let preview = noteArr.join('');
     return `${startUl}${preview}${endUl}`;
-  }
-}
+  };
+};
